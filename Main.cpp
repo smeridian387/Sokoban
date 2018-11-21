9a17bbf3e2e294096ef9f14d9542ae60c91be299
DarkShinobi100 <37442282+DarkShinobi100@users.noreply.github.com>
2018-11-21 13:53:24 +0000

    Revert "Set Up Base Code From Previous Games"
    This reverts commit 0a2f52f244d09b2c6bfc3f21709ca580fa65e0e2.

--- Main.cpp
+++ Main.cpp
@@ -1,87 +1,22 @@
-// Library Includes
-#include <SFML/Window.hpp>	
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <string>
-#include <cstdlib>
-#include <ctime>
-
-// Project Includes
-#include "AssetManager.h"
-#include "Level.h"
-
-// The main() Function - entry point for our program
 int main()
 {
-	// -----------------------------------------------
-	// Game Setup
-	// -----------------------------------------------
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Cyan);
 
-	// Window - to draw to the screen
-	sf::RenderWindow gameWindow;
-	gameWindow.create(sf::VideoMode::getDesktopMode(), "Maze Escape", sf::Style::Titlebar | sf::Style::Close);
-
-	// Seed our random number generator 
-	srand(time(NULL));
-
-	// Create AssetManager
-	AssetManager assets;
-
-	// Game Clock - to keep track of time passed each frame
-	sf::Clock gameClock;
-
-	//Create the game level
-	Level OurLevel;
-
-	// -----------------------------------------------
-	// Game Loop
-	// -----------------------------------------------
-	while (gameWindow.isOpen())
+	while (window.isOpen())
 	{
-		// -----------------------------------------------
-		// Input Section
-		// -----------------------------------------------
-
-		// Check all events since the last frame and process 
-		// each one until there are no more
-		sf::Event gameEvent;
-		while (gameWindow.pollEvent(gameEvent))
+		sf::Event event;
+		while (window.pollEvent(event))
 		{
-			// TODO: Pass event to input objects
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
 
-
-			// Did the player try to close the window?
-			if (gameEvent.type == sf::Event::Closed)
-			{
-				// If so, call the close function on the window.
-				gameWindow.close();
-			}
-
-		} // End event polling loop
-
-		// -----------------------------------------------
-		// Update Section
-		// -----------------------------------------------
-		// Get the time passed since the last frame and restart our game clock
-		sf::Time FrameTime = gameClock.restart();
-
-		//pass update to level
-		OurLevel.Update(FrameTime);
-
-		// -----------------------------------------------
-		// Draw Section
-		// -----------------------------------------------
-
-		// Clear the window to a single colour
-		gameWindow.clear(sf::Color::Black);
-
-		//pass Display to level
-		OurLevel.Draw(gameWindow);
-
-		// Display the window contents on the screen
-		gameWindow.display();
-
-	} // End of Game Loop
 	return 0;
-
-} // End of main() Function
+}
